41ea70d9d8c8a3576a55e957850e95a6eaa3e0da
shubham jhan <shubhamjha03102003@gmail.com>
2025-07-14 13:10:15 +0530

    utilities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
 dotenv.config()
 
-connectDB();
+connectDB()
+.than(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`server is ruuning on port${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("database connection error !!", err);
+})
